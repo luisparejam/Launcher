0a634d60587695af749ad154fc4d97644916e1e3
Luis Pareja <lpareja@hotmail.com>
2025-01-22 17:16:26 -0400

    Initial commit

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.IO
+Imports System.Data.Odbc
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim s As String
+        If Environment.GetCommandLineArgs.Length > 1 Then
+            s = Environment.GetCommandLineArgs(1)
+            WebBrowser1.Navigate(s)
+            WaitForPageLoad()
+        End If
+        Me.Close()
+    End Sub
+
+    Private Property pageready As Boolean = False
+
+#Region "Page Loading Functions"
+    Private Sub WaitForPageLoad()
+        AddHandler WebBrowser1.DocumentCompleted, New WebBrowserDocumentCompletedEventHandler(AddressOf PageWaiter)
+        While Not pageready
+            Application.DoEvents()
+        End While
+        pageready = False
+    End Sub
+
+    Private Sub PageWaiter(ByVal sender As Object, ByVal e As WebBrowserDocumentCompletedEventArgs)
+        If WebBrowser1.ReadyState = WebBrowserReadyState.Complete Then
+            pageready = True
+            RemoveHandler WebBrowser1.DocumentCompleted, New WebBrowserDocumentCompletedEventHandler(AddressOf PageWaiter)
+        End If
+    End Sub
+#End Region
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+
+    End Sub
+End Class
